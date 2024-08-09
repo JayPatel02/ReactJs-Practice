9e251e6c64f1197f0d3f0c8bad46351709966980
JayPatel02 <jaypately08@gmail.com>
2024-08-09 16:37:10 +0530

    Add redux Todo

---
+++ todoRedux/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Todofrom from './components/Todofromq'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <Todofrom/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
